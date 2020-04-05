40de36d1cf5eecff5e80180da9d14bd70e4a160a
linh <linhknife796@gmail.com>
2020-04-05 23:51:35 +0700

    test messenger

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -10,13 +10,12 @@ import Header from '../header/Index';
 import ContentRoute from '../../routes/ContentRoute';
 import Footer from '../footer/Index';
 import BackToTop from '../backtotop/Index';
-import Messenger from '../messenger/Index';
+
 
 
 function App() {
   return (
     <Router>
-        
         <Header />
         <ContentRoute />
         <Footer />
