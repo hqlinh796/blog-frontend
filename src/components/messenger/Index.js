1b2d208ae44ed18556b0e28019a3d24ef3c49788
linh <linhknife796@gmail.com>
2020-04-10 21:54:43 +0700

    fix messenger

--- src/components/messenger/Index.js
+++ src/components/messenger/Index.js
@@ -1,33 +1,15 @@
-import React, { Component } from 'react';
+import React from 'react';
 import MessengerCustomerChat from 'react-messenger-customer-chat';
 
 
-class Messenger extends Component {
-
-    constructor(props) {
-        super(props);
-        this.state = ({
-            isShow: true
-        })
-    }
-    
-    handleClick = () => this.setState({
-        isShow: !this.state.isShow
-    })
-
-    render() {
+const Messenger = () => {
         return (
             <MessengerCustomerChat
                 pageId="110885300571206"
                 appId="577342806204805"
-                shouldShowDialog={this.state.isShow}
+                shouldShowDialog={true}
             />
         );
-    }
-    componentDidMount() {
-        const btn = document.getElementById('facebook');
-        btn.addEventListener('click', () => this.handleClick());
-    }
 }
 
 export default Messenger;
