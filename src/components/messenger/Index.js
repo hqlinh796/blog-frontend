7f484ca29b9340b7a8ad5c1610a96fa6ac23cbb9
linh <linhknife796@gmail.com>
2020-04-10 21:04:00 +0700

    fix messenger

--- src/components/messenger/Index.js
+++ src/components/messenger/Index.js
@@ -1,26 +1,25 @@
 import React, {useState} from 'react';
-// import MessengerCustomerChat from 'react-messenger-customer-chat';
+import MessengerCustomerChat from 'react-messenger-customer-chat';
 
 
-// const Messenger = props => {
-//      const [isShow, setShow] = useState(true);
-//         return (
-//             <MessengerCustomerChat
-//                 pageId="110885300571206"
-//                 appId="577342806204805"
-//             />
-//         );
-    
-// }
-
-import { FacebookProvider, SendToMessenger } from 'react-facebook';
- 
-export const Messenger = () => {
-    return (
-      <FacebookProvider appId="577342806204805">
-        <SendToMessenger messengerAppId="577342806204805" pageId="110885300571206"/>
-      </FacebookProvider>    
-    );
+const Messenger = props => {
+     const [isShow, setShow] = useState(true);
+        return (
+            <MessengerCustomerChat
+                pageId="110885300571206"
+                appId="577342806204805"
+            />
+        );
 }
+
+// import { FacebookProvider, SendToMessenger } from 'react-facebook';
+ 
+// export const Messenger = () => {
+//     return (
+//       <FacebookProvider appId="577342806204805">
+//         <SendToMessenger messengerAppId="577342806204805" pageId="110885300571206"/>
+//       </FacebookProvider>    
+//     );
+// }
 
 export default Messenger;
