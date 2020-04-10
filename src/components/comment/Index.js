af4d0b88d54e59f4af89f780bdc68375eab87385
linh <linhknife796@gmail.com>
2020-04-10 17:55:19 +0700

    convert comment to component

--- src/components/comment/Index.js
+++ src/components/comment/Index.js
@@ -1,4 +1,5 @@
 import React, { useEffect } from 'react';
+import { FacebookProvider, Comments } from 'react-facebook';
 
 const Comment = props => {
     useEffect(() => {
@@ -7,9 +8,12 @@ const Comment = props => {
       });
 
     return (
-        <div className="p-tb-10">
-            {/* <div className="fb-comments" data-href={props.location} data-width="100%" data-numposts="5"></div> */}
-        </div>
+        // <div className="p-tb-10">
+        //     {/* <div className="fb-comments" data-href={props.location} data-width="100%" data-numposts="5"></div> */}
+        // </div>
+        <FacebookProvider appId="577342806204805" className="p-tb-10">
+            <Comments href={props.location} />
+      </FacebookProvider>
     );
 };
 
