a7f37ddae689031455a9f8f0205ba19246f187ed
linh <linhknife796@gmail.com>
2020-04-10 17:49:49 +0700

    add onlick in messenger component

--- src/components/messenger/Index.js
+++ src/components/messenger/Index.js
@@ -1,12 +1,15 @@
-import React from 'react';
+import React, {useState} from 'react';
 import MessengerCustomerChat from 'react-messenger-customer-chat';
 
+
 const Messenger = props => {
-     
+     const [isShow, setShow] = useState(true);
         return (
             <MessengerCustomerChat
                 pageId="110885300571206"
                 appId="577342806204805"
+                shouldShowDialog={isShow}
+                onClick={() => setShow(!isShow)}
             />
         );
     
