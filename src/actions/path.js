db2b1634605121111a905c5ca1dc14624a2d6106
Linh <42135412+linhcuoi796@users.noreply.github.com>
2020-04-21 09:37:36 +0700

    Update path.js

--- src/actions/path.js
+++ src/actions/path.js
@@ -1,5 +1,5 @@
-export const postPath = 'https://backend-blog796.herokuapp.com/post';
-export const categoryPath = 'https://backend-blog796.herokuapp.com/category';
+export const postPath = 'https://backend7961.herokuapp.com/post';
+export const categoryPath = 'https://backend7961.herokuapp.com/category';
 
 //export const postPath = 'http://localhost:5500/post';
 //export const categoryPath = 'http://localhost:5500/category';
